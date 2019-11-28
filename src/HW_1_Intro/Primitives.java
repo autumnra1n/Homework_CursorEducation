cbeecd9b407dfbc4858b8b67882ed651e6dfaa6e
sviatoslav <svyatoslav.grinik@gmail.com>
2019-11-28 15:55:09 +0100

    master: modified HW #1(added operation with char)

--- src/HW_1_Intro/Primitives.java
+++ src/HW_1_Intro/Primitives.java
@@ -8,7 +8,9 @@ public class Primitives {
         long l = 1_000_000_000L;
         double d = 127.34d;
         float f = 12.34f;
+        char var = (char) (b * s);
 
         System.out.println((b + s) + "\n" + (s - l) + "\n" + (l * d) + "\n" + (d / f));
+        System.out.println(var);
     }
 }
