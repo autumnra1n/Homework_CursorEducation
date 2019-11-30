bf3e94110e9155f322c3be2ab7e2268458813d26
sviatoslav <svyatoslav.grinik@gmail.com>
2019-11-30 22:10:45 +0100

    homework-2: modified ArithmeticMean, DuplicateValues

--- src/HW_2_Control_flow_constructions/ArithmeticMean.java
+++ src/HW_2_Control_flow_constructions/ArithmeticMean.java
@@ -1,15 +1,17 @@
 package HW_2_Control_flow_constructions;
 
 class ArithmeticMean {
-    int[] array = {10, 224, 300, 128, 4};
+    Integer[] array = {10, 224, 300, 128, 4, 1};
 
     public void returnArithmeticalMean() {
-        float sum = 0;
+        double sum = 0;
 
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
         sum = sum / array.length;
+
+        sum = Math.round(sum * 100.0) / 100.0;
 
         System.out.println("Arithmetic mean of this array: " + sum);
     }
--- src/HW_2_Control_flow_constructions/DuplicateValues.java
+++ src/HW_2_Control_flow_constructions/DuplicateValues.java
@@ -1,16 +1,18 @@
 package HW_2_Control_flow_constructions;
 
 class DuplicateValues {
-    int[] array = {3, 2, 3, 1, 4, 2, 8, 3};
+    Integer[] array = {3, 2, 3, 1, 4, 2, 8, 3};
 
     public void replaceDuplicatedValues() {
         System.out.print("Result: ");
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 3 || array[i] == 2) {
-                if (i != 0 && i != 1) {
-                    array[i] = 0;
+        int i,x;
+        for (i = 0; i < array.length; i++) {
+            for (x = 1 + i; x < array.length; x++){
+                if(array[i].equals(array[x])){
+                    array[x] = 0;
                 }
             }
+
             System.out.print(array[i] + " ");
         }
     }
