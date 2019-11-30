0f6b36fca3039933360a4086679d85dfae6ccc1e
sviatoslav <svyatoslav.grinik@gmail.com>
2019-11-30 18:32:21 +0100

    homework-2: added completed tasks from 3 to 6 inclusive

--- src/HW_2_Control_flow_constructions/MainArrayTest.java
+++ src/HW_2_Control_flow_constructions/MainArrayTest.java
@@ -12,3 +12,8 @@ public class MainArrayTest {
         new DuplicateValues().replaceDuplicatedValues();
     }
 }
+//Output:
+//        Sort values by DESC: 11 7 3 2 1
+//        Sum of all the positive numbers: 73
+//        Arithmetic mean of this array: 133
+//        Result: 3 2 0 1 4 2 8 0
