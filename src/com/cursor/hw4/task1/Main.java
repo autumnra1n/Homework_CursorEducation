a3b559c009cb93eff523dbaecc1cee9edd65fbba
sviatoslav <svyatoslav.grinik@gmail.com>
2019-12-08 21:53:31 +0100

    homework-4: added task 1

--- src/com/cursor/hw4/task1/Main.java
+++ src/com/cursor/hw4/task1/Main.java
@@ -10,15 +10,16 @@ import com.cursor.hw4.task1.spaceshape.SquarePyramid;
 
 public class Main {
     public static void main(String[] args) {
-        Shape[] shapes = {new Circle(1, 2, 20),
+
+        Shape[] geometricShapes = {new Circle(1, 2, 20),
                 new Rectangle(1, 2, 21.53, 27.09),
                 new Triangle(12, 31, 11, 6, 43, 23),
                 new SquarePyramid(5, 5, 25, 30),
                 new Cuboid(3, 5, 10, 20, 30),
                 new Sphere(12, 21, 36.12)};
 
-        for (int i = 0; i < shapes.length; i++) {
-            System.out.println(shapes[i]);
+        for (int i = 0; i < geometricShapes.length; i++) {
+            System.out.println(geometricShapes[i]);
         }
     }
 }
